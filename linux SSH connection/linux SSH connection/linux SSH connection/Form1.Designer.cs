483cab297c0088cec9d42bea873e2a9c73392993
Tomer Dobershtein <54745622+tomershtein@users.noreply.github.com>
2020-08-23 09:48:00 +0300

    connection via Linux

---
+++ linux SSH connection/linux SSH connection/linux SSH connection/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace linux_SSH_connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SSHPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SSHUsernameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.IPTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.FilenameComboBox = new System.Windows.Forms.ComboBox();
+            this.Sampel_RateComboBox = new System.Windows.Forms.ComboBox();
+            this.Center_FrequencyComboBox = new System.Windows.Forms.ComboBox();
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.PowerComboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Consoletext = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.SSHPasswordTextBox);
+            this.groupBox1.Controls.Add(this.SSHUsernameTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.PortTextBox);
+            this.groupBox1.Controls.Add(this.IPTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(589, 185);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection to SSH";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // SSHPasswordTextBox
+            // 
+            this.SSHPasswordTextBox.Location = new System.Drawing.Point(130, 140);
+            this.SSHPasswordTextBox.Name = "SSHPasswordTextBox";
+            this.SSHPasswordTextBox.Size = new System.Drawing.Size(119, 20);
+            this.SSHPasswordTextBox.TabIndex = 8;
+            this.SSHPasswordTextBox.Text = "analog";
+            this.SSHPasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // SSHUsernameTextBox
+            // 
+            this.SSHUsernameTextBox.Location = new System.Drawing.Point(130, 99);
+            this.SSHUsernameTextBox.Name = "SSHUsernameTextBox";
+            this.SSHUsernameTextBox.Size = new System.Drawing.Size(119, 20);
+            this.SSHUsernameTextBox.TabIndex = 7;
+            this.SSHUsernameTextBox.Text = "analog";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(6, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SSH Password -";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SSH Username -";
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(234, 53);
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(70, 20);
+            this.PortTextBox.TabIndex = 4;
+            // 
+            // IPTextBox
+            // 
+            this.IPTextBox.Location = new System.Drawing.Point(31, 53);
+            this.IPTextBox.Name = "IPTextBox";
+            this.IPTextBox.Size = new System.Drawing.Size(176, 20);
+            this.IPTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(231, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(28, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Filename";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Sample_Rate";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Center_Frequency";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(252, 31);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Power";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(252, 58);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(26, 13);
+            this.label.TabIndex = 5;
+            this.label.Text = "Port";
+            // 
+            // FilenameComboBox
+            // 
+            this.FilenameComboBox.DisplayMember = "1";
+            this.FilenameComboBox.FormattingEnabled = true;
+            this.FilenameComboBox.Items.AddRange(new object[] {
+            "Fs61.44_cw_0M.bin"});
+            this.FilenameComboBox.Location = new System.Drawing.Point(103, 28);
+            this.FilenameComboBox.Name = "FilenameComboBox";
+            this.FilenameComboBox.Size = new System.Drawing.Size(121, 21);
+            this.FilenameComboBox.TabIndex = 6;
+            this.FilenameComboBox.Tag = "";
+            // 
+            // Sampel_RateComboBox
+            // 
+            this.Sampel_RateComboBox.FormattingEnabled = true;
+            this.Sampel_RateComboBox.Items.AddRange(new object[] {
+            "61.44"});
+            this.Sampel_RateComboBox.Location = new System.Drawing.Point(103, 55);
+            this.Sampel_RateComboBox.Name = "Sampel_RateComboBox";
+            this.Sampel_RateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.Sampel_RateComboBox.TabIndex = 7;
+            // 
+            // Center_FrequencyComboBox
+            // 
+            this.Center_FrequencyComboBox.FormattingEnabled = true;
+            this.Center_FrequencyComboBox.Items.AddRange(new object[] {
+            "1500"});
+            this.Center_FrequencyComboBox.Location = new System.Drawing.Point(103, 82);
+            this.Center_FrequencyComboBox.Name = "Center_FrequencyComboBox";
+            this.Center_FrequencyComboBox.Size = new System.Drawing.Size(121, 21);
+            this.Center_FrequencyComboBox.TabIndex = 8;
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Items.AddRange(new object[] {
+            "A"});
+            this.PortComboBox.Location = new System.Drawing.Point(305, 55);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(121, 21);
+            this.PortComboBox.TabIndex = 9;
+            // 
+            // PowerComboBox
+            // 
+            this.PowerComboBox.FormattingEnabled = true;
+            this.PowerComboBox.Items.AddRange(new object[] {
+            "20"});
+            this.PowerComboBox.Location = new System.Drawing.Point(305, 28);
+            this.PowerComboBox.Name = "PowerComboBox";
+            this.PowerComboBox.Size = new System.Drawing.Size(121, 21);
+            this.PowerComboBox.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Activate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.PowerComboBox);
+            this.groupBox2.Controls.Add(this.PortComboBox);
+            this.groupBox2.Controls.Add(this.Center_FrequencyComboBox);
+            this.groupBox2.Controls.Add(this.Sampel_RateComboBox);
+            this.groupBox2.Controls.Add(this.FilenameComboBox);
+            this.groupBox2.Controls.Add(this.label);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(14, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(585, 154);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Command";
+            // 
+            // Consoletext
+            // 
+            this.Consoletext.Location = new System.Drawing.Point(6, 19);
+            this.Consoletext.Multiline = true;
+            this.Consoletext.Name = "Consoletext";
+            this.Consoletext.Size = new System.Drawing.Size(446, 327);
+            this.Consoletext.TabIndex = 13;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Consoletext);
+            this.groupBox3.Location = new System.Drawing.Point(613, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(458, 352);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Console";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 363);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox SSHPasswordTextBox;
+        private System.Windows.Forms.TextBox SSHUsernameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PortTextBox;
+        private System.Windows.Forms.TextBox IPTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ComboBox FilenameComboBox;
+        private System.Windows.Forms.ComboBox Sampel_RateComboBox;
+        private System.Windows.Forms.ComboBox Center_FrequencyComboBox;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.Windows.Forms.ComboBox PowerComboBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Consoletext;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ linux SSH connection/linux SSH connection/linux SSH connection/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Net;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+
+namespace linux_SSH_connection
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            IPTextBox.Text = "127.0.0.1";
+            PortTextBox.Text = "7000";
+            label5.Text = "Not Connected";
+            FilenameComboBox.Text = "Not Selected";
+            Sampel_RateComboBox.Text = "Not Selected";
+            Center_FrequencyComboBox.Text = "Not Selected";
+            PowerComboBox.Text = "Not Selected";
+            PortComboBox.Text = "Not Selected";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SshClient client = new SshClient(IPTextBox.Text, PortTextBox.Text);
+            client.Connect();
+            Consoletext.AppendText("Connected" + "\n");
+            string ch = Consoletext.Text;
+            Byte[] message = Encoding.Unicode.GetBytes(ch);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Consoletext.AppendText("curl -XPOST -H " + "Content - Type: application / json" + "127.0.0.1:8080/api/tx_on_external_file -d " + ("\n" + FilenameComboBox.Text ) + (Sampel_RateComboBox.Text )) ;
+            string ch = Consoletext.Text;
+
+        }
+
+    }
+}
